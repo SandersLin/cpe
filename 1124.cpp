b18d3c6bfee712bd3f5d34f6b11ff5bbe8837113
Sanders Lin <45224617+SandersLin@users.noreply.github.com>
2020-08-07 16:09:26 +0800

    Create 1124.cpp

---
+++ 1124.cpp
@@ -0,0 +1,16 @@
+//just echo 
+
+#include <iostream>
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main()
+{
+    char c; 
+    while( (c= getchar()) != EOF){
+        putchar(c);
+    }
+
+    return 0;
+}
